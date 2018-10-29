d12aa18d1475235c82ebf410dc3b152a63a9d1fe
ttowncompiled <ian.riley.915@gmail.com>
2018-10-29 08:56:16 -0500

    Adds AST interfaces and Program root node

---
+++ rose/src/ast/mod.rs
@@ -0,0 +1,39 @@
+pub trait Node: ToString {
+    fn token_literal(&self) -> Option<String>;
+}
+
+pub trait Statement: Node {}
+pub trait Expression: Node {}
+
+pub struct Program {
+    pub statements:     Vec<Box<dyn Statement>>,
+}
+
+impl Program {
+    pub fn new() -> Program {
+        return Program{
+            statements: Vec::new(),
+        };
+    }
+}
+
+impl ToString for Program {
+    fn to_string(&self) -> String {
+        let mut builder: String = String::new();
+        for stmt in self.statements.iter() {
+            builder.push_str(&(**stmt).to_string());
+        }
+        return builder;
+    }
+}
+
+impl Node for Program {
+    fn token_literal(&self) -> Option<String> {
+        if self.statements.len() > 0 {
+            return self.statements[0].token_literal();
+        } else {
+            return None;
+        }
+    }
+}
+
